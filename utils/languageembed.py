cc9864094901b7c74edfececc475c6c28a00eaa2
DeepKungChannel <ppoumppoum@gmail.com>
2021-12-27 14:28:46 +0700

    Update languageembed.py

--- utils/languageembed.py
+++ utils/languageembed.py
@@ -1,10 +1,11 @@
 import discord
+import settings
 
 class languageEmbed():
     def languageembed(self , ctx):
         embed = discord.Embed(
             title = "Language setting / ตั้งค่าภาษา",
-            description = "```คุณต้องตั้งค่าภาษาก่อน / You need to set the language first```" + "\n" + "/r setlanguage thai : เพื่อตั้งภาษาไทย" + "\n" + "/r setlanguage english : To set English language"
+            description = "```คุณต้องตั้งค่าภาษาก่อน / You need to set the language first```" + "\n" + f"{settings.COMMAND_PREFIX} setlanguage thai : เพื่อตั้งภาษาไทย" + "\n" + f"{settings.COMMAND_PREFIX} setlanguage english : To set English language"
 
         )
         embed.set_footer(text=f"┗Requested by {ctx.author}")
