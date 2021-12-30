7b40b38fe28bb9be3602d6516b7b0a82455a06c7
deepkung1904 <ppoumppoum@gmail.com>
2021-12-30 07:41:34 +0700

    Adding mode in scram.py

--- cogs/scram.py
+++ cogs/scram.py
@@ -18,6 +18,18 @@ async def get_link_bypassing(url):
         async with session.get(url,allow_redirects=True) as resp:
             return await resp.text()
 
+async def message_handle(message):
+    # mode_data = setting.collectionlsjflsjflsjfdb.find_one({"guild_id": message.guild.id})
+    mode_data = get_mode_data(message.guild.id)
+    mode = mode_data["mode"]
+    if mode == "warn":
+        await message.channel.send(f"{message.author.mention} Please do not send the scam links!")
+    elif mode == "delete":
+        await message.delete()
+        await message.channel.send(f"{message.author.mention} Please do not send the scam links!")
+
+
+
 async def check_scram_link(self,message):
     if message.author.bot:
         return
@@ -28,16 +40,15 @@ async def check_scram_link(self,message):
         b_link = await get_link_bypassing(link.group("url"))
         domain = await get_domain_name_from_url(b_link)
         if domain in scram_link or domain in scram_link2:
-            await message.delete()
-            await message.channel.send(f"{message.author.mention} Please do not send a scam link here.")
+            await message_handle(message)
 
-        print(domain)
     else:
-        pass
-        # for content in message.content.split():
-        #     if content in scram_link or content in scram_link2:
-        #         await message.delete()
-        #         await message.channel.send(f"{message.author.mention} Please do not send the scam links!")
+        #if link not found in message
+        #example https://www.youtube.com/ == Found
+        #example www.youtube.com == Not Found    this block will  try www.youtube.com is in the list or not
+        for content in message.content.split():
+            if content in scram_link or content in scram_link2:
+                await message_handle(message)
 
     
     print("run link")
@@ -88,15 +99,32 @@ class Scram(commands.Cog):
 
 
     @scram.command()
+    async def mode(self,ctx,mode):
+        _fetch_data = get_mode_data(ctx.guild.id) #This line will make sure that guild is in the database
+        if mode == "warn":
+            # await settings.sdfsdfsdfsdf.update_one({"guild_id":ctx.guild.id},{'$set':{"mode":"warn"}})
+            await ctx.send(f"{ctx.author.mention} Set mode to warn")
+        elif mode == "delete":
+            # await settings.sdfsdfsdfs.update_one({"guild_id":ctx.guild.id},{'$set':{"mode":"delete"}})
+            await ctx.send(f"{ctx.author.mention} Set mode to delete")
+
+    @scram.command()
     async def add(self,ctx,link):
         pass
-
-
+    
     @scram.command()
     async def remove(self,ctx,link):
         pass
-        
 
+async def get_mode_data(guild_id):
+    # data = settings.clsjflsjflsjflsjfdb.find_one({"guild_id": guild_id})
+    # if data == None:
+    #     settings.clsjflsjflsjflsjfdb.insert_one({"guild_id": guild_id, "mode": "warn"})
+    #     return settings.clsjflsjflsjflsjfdb.find_one({"guild_id": guild_id})
+    # else:
+    #     return data
+    data = {"_id":"293874293423","guild_id": "293874293423","mode": "warn"}
+    return data
 
 async def get_domain_name_from_url(url):
     return url.split("//")[-1].split("/")[0]
