154100bd6be3d1b44fc93d2633797e31652e5aa8
Anapah <70201574+reactxsw@users.noreply.github.com>
2022-02-08 23:17:22 +0700

    update

--- utils/language/translate.py
+++ utils/language/translate.py
@@ -3,36 +3,122 @@ import json
 with open('language.json', encoding="utf8") as file:
     language = json.load(file)
 
-class animelang():
-    def __init__(self):
+class translate_anime():
+    def call():
         return language["Anime"]
 
-botsetuplang = language["Botsetup"]
-discordactivitylang = language["Discordactivity"]
-discordinfolang = language["Discordinfo"]
-donatelang = language["Donate"]
-economylang = language["Economy"]
-errorlang = language["Error"]
-eventlang = language["Event"]
-faceitlang = language["Faceit"]
-funlang = language["Fun"]
-gamblelang = language["Gamble"]
-gamelang = language["Game"]
-gameinfolang = language["Gameinfo"]
-generallang = language["General"]
-helplang = language["Help"]
-imagelang = language["Image"]
-infolang = language["Info"]
-levellang = language["Level"]
-modlang = language["Mod"]
-musiclang = language["Music"]
-nsfwlang = language["Nsfw"]
-onmessagelang = language["OnMessage"]
-proxylang = language["Proxy"]
-reactrolelang = language["Reactrole"]
-scamlang = language["Scam"]
-serverstatlang = language["Serverstat"]
-setlanguagelang = language["Setlanguage"]
-shortenerlang = language["Shortener"]
-tictactoelang = language["Tictactoe"]
-verifylang = language["Verify"]
+class translate_botseup():
+    def call():
+        return language["Botsetup"]
+
+class translate_discordactivity():
+    def call():
+        return language["Discordactivity"]
+
+class translate_discordinfo():
+    def call():
+        return language["Discordinfo"]
+
+class translate_donate():
+    def call():
+        return language["Donate"]
+
+class translate_economy():
+    def call():
+        return language["Economy"]
+
+class translate_error():
+    def call():
+        return language["Error"]
+    
+class translate_event():
+    def call():
+        return language["Event"]
+
+class translate_faceit():
+    def call():
+        return language["Faceit"]
+
+class translate_fun():
+    def call():
+        return language["Fun"]
+
+class translate_gamble():
+    def call():
+        return language["Gamble"]
+
+class translate_game():
+    def call():
+        return language["Game"]
+
+class translate_gameinfo():
+    def call():
+        return language["Gameinfo"]
+
+class translate_general():
+    def call():
+        return language["General"]
+
+class translate_help():
+    def call():
+        return language["Help"]
+
+class translate_image():
+    def call():
+        return language["Image"]
+
+class translate_info():
+    def call():
+        return language["Info"]
+    
+class translate_level():
+    def call():
+        return language["Level"]
+
+class translate_mod():
+    def call():
+        return language["Mod"]
+
+class translate_music():
+    def call():
+        return language["Music"]
+
+class translate_music():
+    def call():
+        return language["Nsfw"]
+
+class translate_onmessage():
+    def call():
+        return language["OnMessage"]
+
+class translate_proxy():
+    def call():
+        return language["Proxy"]
+
+class translate_reactrole():
+    def call():
+        return language["Reactrole"]
+
+class translate_scam():
+    def call():
+        return language["Scam"]
+
+class translate_setserverstat():
+    def call():
+        return language["Serverstat"]
+
+class translate_setlanguage():
+    def call():
+        return language["Setlanguage"]
+    
+class translate_shortener():
+    def call():
+        return language["Shortener"]
+
+class translate_tictactoe():
+    def call():
+        return language["Tictactoe"]
+
+class translate_verify():
+    def call():
+        return language["Verify"]
